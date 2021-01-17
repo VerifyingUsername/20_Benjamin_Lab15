74f93e24e8356316fa227d9807922929ed104ecd
VerifyingUsername <72852459+VerifyingUsername@users.noreply.github.com>
2021-01-17 21:04:12 +0800

    Initial commit

---
+++ Assets/Scenes/BGM.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    public AudioClip[] BGMclipArr;
+
+    private AudioSource bgmSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgmSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bgm();
+
+        
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            bgmSource.volume += 0.1f * Time.deltaTime;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            bgmSource.volume -= 0.1f * Time.deltaTime;
+        }
+    }
+
+    private void bgm ()
+    {
+        int rand = Random.Range(0, BGMclipArr.Length);
+        bgmSource.PlayOneShot(BGMclipArr[rand]);
+    }
+}
---
+++ Assets/Scenes/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject jumpText;
+    public AudioClip[] AudioClipArr;
+
+    private float jump = 5.0f;
+
+    public Rigidbody playerRb;
+
+    private int jumpCounter;
+
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpCounter++;
+
+            jumpText.GetComponent<Text>().text = "Jump: " + jumpCounter;
+
+            playerRb.AddForce(new Vector3(0, 250, 0));
+
+            //audioSource.Play();
+            int rand = Random.Range(0, AudioClipArr.Length);
+            audioSource.PlayOneShot(AudioClipArr[rand]);
+        }
+    }
+}
